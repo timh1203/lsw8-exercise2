a1774d5b660e1b73e494ee416f842bf04ceba111
Timothy Hoang <timh1203@gmail.com>
2018-09-10 21:14:07 -0400

    Add app component and react router

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Lambda School Week 8: React Router I (Tyler McGinnis)
 
 [Nested routes with React Router v4](https://tylermcginnis.com/react-router-nested-routes/)
+[Full Example](https://csb-v8k006wnol-xdwmyvgvdg.now.sh/)
--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div style={{width: 1000, margin: '0 auto'}}>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
